f772aa2396155596936c390a4a9ee55fc69522fd
LeLoc1712 <levuvinhloc.vn@gmail.com>
2026-03-08 16:13:34 +0700

    save book

---
+++ LibraryAPI/Controllers/UserLibraryController.cs
@@ -0,0 +1,50 @@
+﻿using LibraryAPI.Service.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize] // Bắt buộc user phải đăng nhập mới được gọi API này
+    public class UserLibraryController : ControllerBase
+    {
+        private readonly IUserLibraryService _libraryService;
+
+        public UserLibraryController(IUserLibraryService libraryService)
+        {
+            _libraryService = libraryService;
+        }
+
+        [HttpPost("{bookId}/toggle-save")]
+        public async Task<IActionResult> ToggleSave(int bookId)
+        {
+            var userIdStr = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (!int.TryParse(userIdStr, out int userId)) return Unauthorized();
+
+            var isSaved = await _libraryService.ToggleFavoriteAsync(userId, bookId);
+            return Ok(new { isSaved = isSaved });
+        }
+
+        [HttpGet("saved")]
+        public async Task<IActionResult> GetSavedBooks()
+        {
+            var userIdStr = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (!int.TryParse(userIdStr, out int userId)) return Unauthorized();
+
+            var books = await _libraryService.GetSavedBooksAsync(userId);
+            return Ok(books);
+        }
+
+        [HttpGet("{bookId}/check-saved")]
+        public async Task<IActionResult> CheckIsSaved(int bookId)
+        {
+            var userIdStr = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (!int.TryParse(userIdStr, out int userId)) return Unauthorized();
+
+            var isSaved = await _libraryService.CheckIsSavedAsync(userId, bookId);
+            return Ok(new { isSaved = isSaved });
+        }
+    }
+}
---
+++ LibraryAPI/Service/Interface/IUserLibraryService.cs
@@ -0,0 +1,11 @@
+﻿using LibraryAPI.DTOs;
+
+namespace LibraryAPI.Service.Interface
+{
+    public interface IUserLibraryService
+    {
+        Task<bool> ToggleFavoriteAsync(int userId, int bookId);
+        Task<List<BookResponse>> GetSavedBooksAsync(int userId);
+        Task<bool> CheckIsSavedAsync(int userId, int bookId);
+    }
+}
