e946865b240086a28c8bb2a3aa9d04eec946d5b1
reality <anhnvhe187352@fpt.edu.vn>
2026-03-08 19:32:09 +0700

    feat(api): update GetBook method

--- LibraryAPI/Service/Interface/IAdminBookService.cs
+++ LibraryAPI/Service/Interface/IAdminBookService.cs
@@ -4,6 +4,7 @@ namespace LibraryAPI.Service.Interface
 {
     public interface IAdminBookService
     {
+        Task<PagedResult<BookResponse>> GetBooksAsync(int page = 1, int pageSize = 20, string? searchTerm = null);
         Task<CreateBookResponse> CreateBookAsync(CreateBookRequest request);
         Task<BookResponse?> GetBookByIdAsync(int bookId);
         Task<BookResponse?> UpdateBookAsync(int bookId, UpdateBookRequest request);
